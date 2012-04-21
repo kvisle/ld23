3a34caf6b18daa86beb7576111bc7c50f98a51db
Trygve Vea <trygve.vea@gmail.com>
2012-04-22 00:17:53 +0200

    foo

--- src/game.cpp
+++ src/game.cpp
@@ -46,7 +46,7 @@ game::game() : c(160, 144, 160, 144), gs(5)
     assets.push_back(new key(this, 82*8, 68*8, 0));
     assets.push_back(new door(this, 156*8, 30*8, 0));
     assets.push_back(new door(this, 182*8, 66*8, 0));
-    assets.push_back(new door(this, 176*8, 85*8, 0));
+    assets.push_back(new door(this, 176*8, 84*8, 0));
 
 
     std::cout << "Made game" << std::endl;
